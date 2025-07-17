b9561df3b8dc4098b778df376ab1cae458bfe6a0
Phil-tech869 <122480911+Phil-tech869@users.noreply.github.com>
2025-07-17 21:15:44 +0200

    Add App.jsx inside src/

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="p-4 text-center text-xl">
+      🎰 Gambling Prototype – Ready for Vercel!
+    </div>
+  );
+}
